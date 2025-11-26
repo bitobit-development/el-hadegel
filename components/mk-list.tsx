5459f3dde625f18d92b4d56f1f36ef890570f99b
Haim Derazon <haim@bit2bit.co.za>
2025-11-26 15:23:36 +0200

    fix: Use MKDataWithTweetCount type in MKList component

--- components/mk-list.tsx
+++ components/mk-list.tsx
@@ -3,10 +3,10 @@
 import { useState, useMemo } from 'react';
 import { MKCard } from '@/components/mk-card';
 import { FilterPanel } from '@/components/filter-panel';
-import { MKData, PositionStatus } from '@/types/mk';
+import { MKDataWithTweetCount, PositionStatus } from '@/types/mk';
 
 interface MKListProps {
-  mks: MKData[];
+  mks: MKDataWithTweetCount[];
   factions: string[];
 }
 
