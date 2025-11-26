0821135aa19acdfe163deda6d2b2b54ef1b39e8f
Haim Derazon <haim@bit2bit.co.za>
2025-11-26 16:08:09 +0200

    feat: Add Knesset image domain to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fs.knesset.gov.il',
+        pathname: '/globaldocs/MK/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
